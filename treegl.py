ac098a64a1ec1f43e0a6a71f91afefe09c0356f7
RagDoll95 <raghuveergummadi9@gmail.com>
2023-02-15 20:59:54 -0500

    some values

--- treegl.py
+++ treegl.py
@@ -1,1 +1,12 @@
 from ete3 import Tree
+
+#hardcoding these values for now
+TREE_FILE = 'test.nh'
+GAINS = 1
+LOSSES = 1
+
+t = Tree(TREE_FILE)
+
+branches = len(t.get_tree_root())
+
+print(branches)
